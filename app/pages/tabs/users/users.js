ee80ba19e03b3d9d7fc98161a457dc18c4856a37
Siva_Tumma <abhilash.r@westagilelabs.com>
2017-09-14 02:03:11 +0530

    siva:: latest changes

--- app/pages/tabs/users/users.js
+++ app/pages/tabs/users/users.js
@@ -1,5 +1,5 @@
 angular.module('myApp.dashboard')
-    .controller('UsersController', ['$scope', 'utility', 'UsersService', function($scope, utility, usersService) {
+    .controller('UsersController', ['$scope', 'utility', 'UsersService', function ($scope, utility, usersService) {
         $scope.userRole = utility.getUserRole();
         $scope.viewby = 10;
         $scope.currentPage = 1;
@@ -14,39 +14,39 @@ angular.module('myApp.dashboard')
 
         $scope.dropDownValues = { 'UserStatus': [], 'UserTypes': [] };
 
-        $scope.pageChanged = function() {
+        $scope.pageChanged = function () {
             console.log('Page changed to: ' + $scope.currentPage);
         };
-        $scope.setItemsPerPage = function(num) {
+        $scope.setItemsPerPage = function (num) {
             $scope.itemsPerPage = num;
             $scope.currentPage = 1; //reset to first page
         }
 
-        $scope.isAddAvailable = function() {
+        $scope.isAddAvailable = function () {
             if ($scope.userRole === 'Super Admin' || $scope.userRole === 'Admin')
                 return true;
             else
                 return false;
         }
 
-        $scope.loadAllUsers = function() {
-            usersService.getUsersList().then(function(response) {
+        $scope.loadAllUsers = function () {
+            usersService.getUsersList().then(function (response) {
                 $scope.usersList = response.data;
                 $scope.totalItems = $scope.usersList.length;
-            }, function(error) {});
+            }, function (error) { });
         }
 
-        $scope.loadDropdownsData = function() {
+        $scope.loadDropdownsData = function () {
             //alert("load user static data")
-            usersService.getStaticUserData().then(function(response) {
+            usersService.getStaticUserData().then(function (response) {
                 $scope.dropDownValues.UserTypes = response.data.data.UserTypes;
                 $scope.dropDownValues.UserStatus = response.data.data.UserStatus;
                 console.log(response.data);
-            }, function(error) {});
+            }, function (error) { });
         }
 
 
-        $scope.addNewUser = function() {
+        $scope.addNewUser = function () {
 
             $scope.addClicked = true;
             $scope.errorMessageUserName = false;
@@ -56,31 +56,26 @@ angular.module('myApp.dashboard')
 
         }
 
-        $scope.onCancel = function() {
-                $scope.addClicked = false;
+        $scope.onCancel = function () {
+            $scope.addClicked = false;
+        }
 
-
-            }
-            // for password checking
-            // if ($scope.newUser.rePassword && $scope.newUser.userPassword) {
-            //     $scope.myFunction = function() {
-            //         if ($scope.newUser.rePassword! == $scope.newUser.userPassword) {
-            //             $scope.passwordErrorFlag = true;
-            //         } else {
-            //             $scope.passwordErrorFlag = false;
-            //         }
-            //     }
-            // }
-
-        $scope.saveUser = function(user) {
+        $scope.isPasswordSame = function () {
+                    if ($scope.newUser.rePassword !== '' && $scope.newUser.userPassword !== '' && $scope.newUser.rePassword !== $scope.newUser.userPassword) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+        }
+        $scope.saveUser = function (user) {
             var userType = [];
             userType.push(user.userType);
             var body = { "userName": user.userName, "userFirstName": user.userFirstName, "userLastName": user.userLastName, "userDOB": user.userDOB, "userAadharNum": user.userAadharNum, "userMobileNum": user.userMobileNum, "userPassword": user.userPassword, "userType": userType, "userStatus": user.userStatus };
-            usersService.addUser(body).then(function(success) {
+            usersService.addUser(body).then(function (success) {
                 $scope.newUser = { "userName": "", "userFirstName": "", "userLastName": "", "userDOB": "", "userAadharNum": "", "userMobileNum": "", "userPassword": "", "rePassword": "", "userType": "", "userStatus": "" };
                 $scope.loadAllUsers();
                 $scope.addClicked = false;
-            }, function(error) {
+            }, function (error) {
 
                 $scope.addClicked = true;
                 console.log(error);
