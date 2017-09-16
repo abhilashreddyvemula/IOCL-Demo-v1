3acbcc4905d6f385c9e0377b7ed0defa6b05c949
abhilashreddyvemula <abhilash9998@gmail.com>
2017-09-16 18:45:47 +0530

    changes in IOCL

--- app/pages/tabs/users/users.js
+++ app/pages/tabs/users/users.js
@@ -1,5 +1,5 @@
 angular.module('myApp.dashboard')
-    .controller('UsersController', ['$scope', 'utility', 'UsersService', function ($scope, utility, usersService) {
+    .controller('UsersController', ['$scope', 'utility', 'UsersService', function($scope, utility, usersService) {
         $scope.userRole = utility.getUserRole();
         $scope.viewby = 10;
         $scope.currentPage = 1;
@@ -14,55 +14,55 @@ angular.module('myApp.dashboard')
 
         $scope.dropDownValues = { 'UserStatus': [], 'UserTypes': [] };
 
-        $scope.pageChanged = function () {
+        $scope.pageChanged = function() {
             console.log('Page changed to: ' + $scope.currentPage);
         };
-        $scope.setItemsPerPage = function (num) {
+        $scope.setItemsPerPage = function(num) {
             $scope.itemsPerPage = num;
             $scope.currentPage = 1; //reset to first page
         }
 
-        $scope.isAddAvailable = function () {
+        $scope.isAddAvailable = function() {
             if ($scope.userRole === 'Super Admin' || $scope.userRole === 'Admin')
                 return true;
             else
                 return false;
         }
 
-        $scope.formatDate = function (date) {
+        $scope.formatDate = function(date) {
             function pad(n) {
                 return n < 10 ? '0' + n : n;
             }
-        
-            return date && date.getFullYear()
-                + '-' + pad(date.getMonth() + 1)
-                + '-' + pad(date.getDate());
+
+            return date && date.getFullYear() +
+                '-' + pad(date.getMonth() + 1) +
+                '-' + pad(date.getDate());
         };
-        
-        $scope.parseDate = function (s) {
+
+        $scope.parseDate = function(s) {
             var tokens = /^(\d{4})-(\d{2})-(\d{2})$/.exec(s);
-        
+
             return tokens && new Date(tokens[1], tokens[2] - 1, tokens[3]);
         };
 
-        $scope.loadAllUsers = function () {
-            usersService.getUsersList().then(function (response) {
+        $scope.loadAllUsers = function() {
+            usersService.getUsersList().then(function(response) {
                 $scope.usersList = response.data;
                 $scope.totalItems = $scope.usersList.length;
-            }, function (error) { });
+            }, function(error) {});
         }
 
-        $scope.loadDropdownsData = function () {
+        $scope.loadDropdownsData = function() {
             //alert("load user static data")
-            usersService.getStaticUserData().then(function (response) {
+            usersService.getStaticUserData().then(function(response) {
                 $scope.dropDownValues.UserTypes = response.data.data.UserTypes;
                 $scope.dropDownValues.UserStatus = response.data.data.UserStatus;
                 console.log(response.data);
-            }, function (error) { });
+            }, function(error) {});
         }
 
 
-        $scope.addNewUser = function () {
+        $scope.addNewUser = function() {
 
             $scope.addClicked = true;
             $scope.errorMessageUserName = false;
@@ -72,26 +72,30 @@ angular.module('myApp.dashboard')
 
         }
 
-        $scope.onCancel = function () {
+        $scope.onCancel = function() {
             $scope.addClicked = false;
         }
 
-        $scope.isPasswordSame = function () {
-                    if ($scope.newUser.rePassword !== '' && $scope.newUser.userPassword !== '' && $scope.newUser.rePassword !== $scope.newUser.userPassword) {
-                        return true;
-                    } else {
-                        return false;
-                    }
+        $scope.showPwd = function() {
+            let obj = document.getElementById('myPassword');
+            obj.type = "text";
         }
-        $scope.saveUser = function (user) {
+        $scope.isPasswordSame = function() {
+            if ($scope.newUser.rePassword !== '' && $scope.newUser.userPassword !== '' && $scope.newUser.rePassword !== $scope.newUser.userPassword) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        $scope.saveUser = function(user) {
             var userType = [];
             userType.push(user.userType);
             var body = { "userName": user.userName, "userFirstName": user.userFirstName, "userLastName": user.userLastName, "userDOB": user.userDOB, "userAadharNum": user.userAadharNum, "userMobileNum": user.userMobileNum, "userPassword": user.userPassword, "userType": userType, "userStatus": user.userStatus };
-            usersService.addUser(body).then(function (success) {
+            usersService.addUser(body).then(function(success) {
                 $scope.newUser = { "userName": "", "userFirstName": "", "userLastName": "", "userDOB": "", "userAadharNum": "", "userMobileNum": "", "userPassword": "", "rePassword": "", "userType": "", "userStatus": "" };
                 $scope.loadAllUsers();
                 $scope.addClicked = false;
-            }, function (error) {
+            }, function(error) {
 
                 $scope.addClicked = true;
                 console.log(error);
@@ -114,4 +118,15 @@ angular.module('myApp.dashboard')
 
         $scope.loadAllUsers();
         $scope.loadDropdownsData();
+
+        // function mouseoverPass(obj) {
+        //     var obj = document.getElementById('myPassword');
+        //     obj.type = "text";
+        // }
+
+        // function mouseoutPass(obj) {
+        //     var obj = document.getElementById('myPassword');
+        //     obj.type = "password";
+        // }
+
     }]);
