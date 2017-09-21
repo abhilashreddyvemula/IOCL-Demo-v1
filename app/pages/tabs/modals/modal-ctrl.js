f37b1d025c52334485e1365834a8ab3bce478fae
Siva_Tumma <abhilash.r@westagilelabs.com>
2017-09-21 20:46:57 +0530

    siva:: latest changes

--- app/pages/tabs/modals/modal-ctrl.js
+++ app/pages/tabs/modals/modal-ctrl.js
@@ -1,13 +1,18 @@
 angular.module('myApp.modals', [])
-.controller('BayEditModalCtrl', function ($uibModalInstance, items, BayService) {
+  .controller('BayEditModalCtrl', function ($uibModalInstance, items, BayService) {
     var $ctrl = this;
     $ctrl.bay = items.bay;
     $ctrl.dropDownValues = items.dropDownValues;
-  
-    $ctrl.save = function () {
+
+    $ctrl.update = function () {
       //$uibModalInstance.close($ctrl.selected.item);
+      let bay = { 'bayName': $ctrl.bay.bayName, 'bayNum': $ctrl.bay.bayNum, 'bayType': $ctrl.bay.bayType, 'functionalStatus': $ctrl.bay.functionalStatus, 'editbayNumFlag': true, 'editbayNameFlag': true, 'bayId': $ctrl.bay.bayId }
+      BayService.updateBay(bay).then(function (response) {
+        console.log('Response', response);
+        $ctrl.cancel();
+      });
     };
-  
+
     $ctrl.cancel = function () {
       $uibModalInstance.dismiss('cancel');
     };
@@ -17,11 +22,11 @@ angular.module('myApp.modals', [])
     var $ctrl = this;
     $ctrl.user = items.user;
     $ctrl.dropDownValues = items.dropDownValues;
-    console.log($ctrl.user ,$ctrl.dropDownValues );
+    console.log($ctrl.user, $ctrl.dropDownValues);
     $ctrl.save = function () {
       //$uibModalInstance.close($ctrl.selected.item);
     };
-  
+
     $ctrl.cancel = function () {
       $uibModalInstance.dismiss('cancel');
     };
