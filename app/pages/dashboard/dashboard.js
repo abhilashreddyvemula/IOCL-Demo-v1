84d24d66bcf26534282c7d0ef73c30cea829ea39
Siva_Tumma <abhilash.r@westagilelabs.com>
2017-10-30 21:33:13 +0530

    siva:: latest changes

--- app/pages/dashboard/dashboard.js
+++ app/pages/dashboard/dashboard.js
@@ -19,16 +19,16 @@ angular.module('myApp.dashboard', ['ngRoute', 'ui.bootstrap'])
             $scope.main.active = {};
             if ($scope.userRole === 'Super Admin') {
                 $scope.main.active.tab = 'bay';
-                $scope.userPrivilages = { 'bay': true, 'users': true, 'contractors': true, 'locations': true, 'quantity': true, 'reports': false, 'operator': false, 'baydetails': true };
+                $scope.userPrivilages = { 'bay': true, 'users': true, 'contractors': true, 'locations': true, 'quantity': true, 'reports': false, 'operator': false, 'baydetails': false };
             } else if ($scope.userRole === 'Admin') {
                 $scope.main.active.tab = 'bay';
-                $scope.userPrivilages = { 'bay': true, 'users': true, 'contractors': true, 'locations': true, 'quantity': true, 'reports': true, 'operator': false, 'baydetails': true };
+                $scope.userPrivilages = { 'bay': true, 'users': true, 'contractors': true, 'locations': true, 'quantity': true, 'reports': true, 'operator': false, 'baydetails': false };
             } else if ($scope.userRole === 'Supervisor') {
                 $scope.main.active.tab = 'users';
-                $scope.userPrivilages = { 'bay': false, 'users': true, 'contractors': false, 'locations': false, 'quantity': false, 'operator': true, 'baydetails': false };
+                $scope.userPrivilages = { 'bay': false, 'users': true, 'contractors': false, 'locations': false, 'quantity': false, 'operator': true, 'baydetails': true };
             } else {
                 $scope.main.active.tab = 'operator';
-                $scope.userPrivilages = { 'bay': false, 'users': false, 'contractors': false, 'locations': false, 'quantity': false, 'operator': true, 'baydetails': false };
+                $scope.userPrivilages = { 'bay': false, 'users': false, 'contractors': false, 'locations': false, 'quantity': false, 'operator': true, 'baydetails': true };
             }
         }
 
